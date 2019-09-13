32801ce31262da471d1d33118a30c8f05a9eadf1
Sarah McLaughlin <sarahbmcl@gmail.com>
2019-09-12 19:58:36 -0500

    basic js formatting to initiate code

--- javascript/app_index.js
+++ javascript/app_index.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     $('.carousel').carousel();
     $('.sidenav').sidenav();
+
+    function findUserSongs() {
+
+    };
+
+    findUserSongs();
 });
--- javascript/app_song.js
+++ javascript/app_song.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     $('.carousel').carousel();
     $('.sidenav').sidenav();
+
+    function callSongs() {
+
+    };
+
+    callSongs();
 });
