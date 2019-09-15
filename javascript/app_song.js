bc090dd36cbae075a11e8df9f6b1feed6dd13adf
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-09-14 19:21:39 -0500

    constructor function for avatar

--- javascript/app_song.js
+++ javascript/app_song.js
@@ -1,46 +1,32 @@
 $(document).ready(function () {
+
+
     $('.carousel').carousel();
     $('.sidenav').sidenav();
     $('#add-to-queue-modal').modal();
-
-    //avatar constructor
-    Avatar = function(song, artist, cover){
-        this.song = song;
-        this.artist = artist;
-        this.cover = cover;
-    }
-    Avatar.prototype.createAva = function(){
-    let avaItem = $('<li>');
-    avaItem.attr('class','collection-item avatar');
-    let img = $('<img>');
-
-    img.attr({
-        src : this.cover,
-        class : 'circle'
-    });
-    let songBlock = $('<span>');
-    songBlock.attr('class', 'title');
-    songBlock.text(this.song);
-    let artistBlock = $('<p>');
-    artistBlock.text(this.artist);
-    let btn = $('<button>');
-    btn.attr('class', 'secondary-content btn');
-    let icn = $('<i>');
-    icn.attr('class', 'material-icons add-song');
-    icn.text('add_box');
-    btn.append(icn);
-    avaItem.append([img, songBlock,artistBlock, btn]);
-        $('#song-collection').prepend(avaItem);
-    }
-
-
-
-
+   
 
     // open modal
-    $('#song-collection').on('click', '.btn', function(){
+    $('#song-collection').on('click', '.avatar', function(){
+        let obj =this;
+        let song = obj.data.song;
+        let song = obj.data.artist;
+       console.log(song);
+       console.log(artist);
+        //add to UpNext on close
+        $('#add-to-queue-modal').modal({
+            dismissible: false,
+            onCloseEnd: function(){ 
+            console.log(obj.data)   
+        }
+
+    }); 
+
         $('#add-to-queue-modal').modal('open');
+        
     })
+    
+    
     
 
     // function callSongs() {
