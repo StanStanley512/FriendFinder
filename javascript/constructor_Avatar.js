2a243009554f4a2e3b7b90ef5f8c1d41c55a1465
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-09-14 22:33:27 -0500

    new remove button new toast

--- javascript/constructor_Avatar.js
+++ javascript/constructor_Avatar.js
@@ -1,28 +1,39 @@
+let constructCount = 0;
 Avatar = function(song, artist, cover){
     this.song =song;
     this.artist = artist;
     this.cover = cover;
+
+    songData = this.song;
+    artistData = this.artist;
+    coverData = this.cover;
+
 }
 Avatar.prototype.createAva = function(){
-let avaItem = $('<li>');
-avaItem.attr({
-    class :'collection-item avatar',
-    data: {
-        song: this.song,
-        artist: this.artist
-    }
-});
-let img = $('<img>');
+    let avaItem = $('<li>');
+    avaItem.attr({
+        'class' : 'collection-item avatar',
+        'data-song' : songData,
+        'data-artist' : artistData,
+        'data-cover' : coverData,
+        'id' : 'ava-'+constructCount
+    });
 
-img.attr({
-    src : this.cover,
-    class : 'circle'
-});
-let songBlock = $('<span>');
-songBlock.attr('class', 'title');
-songBlock.text(this.song);
-let artistBlock = $('<p>');
-artistBlock.text(this.artist);
-avaItem.append([img, songBlock,artistBlock]);
+    let img = $('<img>');
+    img.attr({
+        'src' : this.cover,
+        'class' : 'circle', 
+        
+    });
+    let songBlock = $('<span>');
+    songBlock.attr('class', 'title');
+    songBlock.text(this.song);
+    let artistBlock = $('<p>');
+    artistBlock.text(this.artist);
+    avaItem.append([img, songBlock,artistBlock]);
+        
     $('#song-collection').prepend(avaItem);
+    // local storage
+    localStorage.setItem(`Ava-${constructCount}`, avaItem)
+    constructCount++;
 }
