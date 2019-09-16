0d3e4b710b3a89542e1030130acbf32a6898c08d
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-09-16 06:22:56 -0500

    clear variables from avatar constructor function

--- javascript/app_song.js
+++ javascript/app_song.js
@@ -16,16 +16,18 @@ $(document).ready(function () {
 
         // modal btn listeners
         $('#add-to-queue-modal').modal('open');
+        //on click no
         $('#no-add-song').on('click', function(){
             $('#add-to-queue-modal').modal('close')
         });
+        //on click remove
         $('#remove-add-song').on('click', function(){
             console.log('yes');
             console.log(Ava['data-song'])
             //remove Avatar
         });
 
-        //adds song to queue page
+        //on click adds song to queue page
         $('#yes-add-song').on('click', function(){
             console.log('yes');
             //remove Avatar
