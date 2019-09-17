a7104120b50f94dd816a2a283ce7407cf6a5f8b2
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-09-16 21:36:36 -0500

    void commit

--- javascript/app_song.js
+++ javascript/app_song.js
@@ -1,63 +1,37 @@
 $(document).ready(function () {
-    localStorage.clear()
+
 
     $('.carousel').carousel();
     $('.sidenav').sidenav();
     $('#add-to-queue-modal').modal();
+   
 
     // open modal
     $('#song-collection').on('click', '.avatar', function(){
-        let Ava= $(this).attr('id')
-        let song = $(this).attr('data-song');
-        let artist = $(this).attr('data-artist');
-        let cover = $(this).attr('data-cover');
-
-        console.log('yes');
-            console.log(Ava)
-            console.log(song);
-            console.log(artist);
-            console.log(cover);
-        
+        let obj =this;
+        let song = obj.data.song;
+        let song = obj.data.artist;
+       console.log(song);
+       console.log(artist);
         //add to UpNext on close
-        $('.add-to-queue-modal').modal({
+        $('#add-to-queue-modal').modal({
             dismissible: false,
-            onCloseEnd: function(){
-            
+            onCloseEnd: function(){ 
+            console.log(obj.data)   
+        }
 
-            }
-        }); 
+    }); 
+
         $('#add-to-queue-modal').modal('open');
-        $(this).detach() //removes from song-collection need to hold in an array
-   
-    });
-
-        // modal btn listeners
-        //on click remove
-        $('#cancel-add-song').on('click', function(){
-            
-            
-        });
-
-        //on click adds song to queue page
-        $('#okay-add-song').on('click', function(){
-          
-            //send Avatar to upnext with firebase
-           
-        }); 
+        
+    })
     
     
-    //LIST SONGS PSUEDOCODE
+    
+
     // function callSongs() {
     //     let queryURL = ' '
-    //     let genres ="XXXXXXX"
 
-    //      create genre button
-    //      call API based on X
-
-    //      on click, data search
-    //      append songs
-
-    // TO CALL SPOTIFY
     //     $.ajax({
     //         url: queryURL,
     //         method: "GET"
